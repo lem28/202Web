6f04cbf3654199ad5fe11e11642837ba1a46e70b
Luis Montenegro <lem28@njit.edu>
2016-02-10 18:41:53 -0500

    Update 0: New repo, new files. Nothing special. Yet.

--- test.php
+++ test.php
@@ -21,22 +21,11 @@ function agentOrange($word, array $arrayIn = NULL)
   foreach ($arrayIn as $arraySubstance)
   {
     echo __FILE__.":".__LINE__.": $word - $arraySubstance".PHP_EOL;
-    $return .= $ar;
+    $return .= $arraySubstance;
   }
   return $return;
 }
-var_dump($newArray);
-echo __FILE__.":".__LINE__.":array size = ".count($newArray).PHP_EOL;
-foreach ($newArray as $arrayObject)
-{
-  echo "array value:\" $arrayObject".PHP_EOL;
-}
-echo "C style:\n";
-for ($i = 0;$i < count($newArray);$i++)
-{
-  echo "array value: ".$newArray[$i].PHP_EOL;
-}
-$ret = agentOrange("davey in the navy");
+$ret = agentOrange("davy in the navy");
 $ret = agentOrange("davy in the navy",$newArray);
 echo $ret.PHP_EOL;
 echo "Test PHP END".PHP_EOL;
