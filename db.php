4c9d8e3950fb7284734a8978ad1a837fb2d6e32b
Luis Montenegro <lem28@njit.edu>
2016-02-24 17:07:31 -0500

    db.php now creates a table

--- db.php
+++ db.php
@@ -1,18 +1,32 @@
 #!usr/bin/php
 <?php
 
-  $db = new mysqli("localhost","root","herecomesthesnow45","it202");
+	$db = new mysqli("localhost","root","herecomesthesnow45","it202");
 
-  if ($db->connect_errno > 0)
-  {
-    echo __file__.__line__." ERROR: ".$db->connect_error.PHP_EOL;
-    exit(-1);
-  }
-  echo "Connected to database".PHP_EOL;
+	if ($db->connect_errno > 0)
+	{
+		echo __file__.__line__." ERROR: ".$db->connect_error.PHP_EOL;
+		exit(-1);
+	}
+	echo "Connected to database".PHP_EOL;
 
-  $query = "select * from mojave";
-  $results = $db->query($query);
-  print_r($results);
-  $db->close();
+	$query = "select * from mojave";
+	$results = $db->query($query);
+	print_r($results);
+
+	$sql = "CREATE TABLE MyGuests (
+		id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+		firstname VARCHAR(30) NOT NULL,
+		lastname VARCHAR(30) NOT NULL,
+		email VARCHAR(50),
+		reg_date TIMESTAMP
+	)";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Table MyGuests created successfully";
+	} else {
+		echo "Error creating table: " . $conn->error;
+	}
+	$db->close();
 
 ?>
